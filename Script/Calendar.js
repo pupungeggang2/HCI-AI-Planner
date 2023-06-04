89a09bc53d0c4fbb0cd50fa68ccef48035636951
pupungeggang2 <kyongkyong289@outlook.kr>
2023-06-05 03:10:34 +0900

    0.0.3 Dashboard, Weekly Calendar

--- Script/Calendar.js
+++ Script/Calendar.js
@@ -1,6 +1,8 @@
 window.onload = calendarInit;
 window.onbeforeunload = calendarEnd;
 
+const dayName = {0 : 'Sun', 1 : 'Mon', 2 : 'Tue', 3 : 'Wed', 4 : 'Thu', 5 : 'Fri', 6 : 'Sat'};
+
 let account;
 let username;
 let usernameInfo;
@@ -11,6 +13,10 @@ let monthStart;
 let viewMode = 'Week';
 let weekDay = [];
 let monthDay = [];
+let calendarTopElement;
+let calendarTableHeadElement;
+let weekElement;
+let monthElement;
 
 function calendarInit() {
     if (localStorage.getItem('PlanGPT-Account') === null) {
@@ -33,21 +39,23 @@ function calendarInit() {
         return;
     }
 
-    findCalendarDate();
-}
-
-function findCalendarDate() {
     dayCurrent = new Date();
     weekStart = new Date();
     monthStart = new Date();
 
+    findCalendarDate();
+    refreshCalendar();
+}
+
+function findCalendarDate() {
     date = dayCurrent.getDate();
     day = dayCurrent.getDay();
 
-    weekStart.setDate(weekStart.getDate() - day);
-    monthStart.setDate(monthStart.getDate() - date + 1);
+    weekStart = new Date(dayCurrent.getFullYear(), dayCurrent.getMonth(), dayCurrent.getDate());
+    monthStart = new Date(dayCurrent.getFullYear(), dayCurrent.getMonth(), dayCurrent.getDate());
 
     let monthStartDay = monthStart.getDay();
+    weekStart.setDate(weekStart.getDate() - day);
     monthStart.setDate(monthStart.getDate() - monthStartDay);
 
     weekDay = [];
@@ -62,6 +70,112 @@ function findCalendarDate() {
         monthDay.push([monthStart.getFullYear(), monthStart.getMonth() + 1, monthStart.getDate(), monthStart.getDay()]);
         monthStart.setDate(monthStart.getDate() + 1);
     }
+}
+
+function refreshCalendar() {
+    calendarTopElement = document.getElementById('calendartop');
+    calendarTableHeadElement = document.getElementById('calendarweeklytableheadview');
+    weekElement = document.getElementById('calendarweeklytableview');
+    monthElement = document.getElementById('calendarmonthlytableview');
+
+    if (viewMode === 'Week') {
+        weekElement.style.display = 'block';
+        monthElement.style.display = 'none';
+
+        calendarTopElement.innerHTML = '';
+        calendarTableHeadElement.innerHTML = '';
+        weekElement.innerHTML = '';
+        monthElement.innerHTML = '';
+
+        // Calendar Top Elmement
+        calendarTopElement.innerHTML += `<button id="calendarleftbutton" onclick="goPreviousWeek();"></button>`;
+        calendarTopElement.innerHTML += `<div id="calendardate">${weekDay[0][0]}/${weekDay[0][1]}/${weekDay[0][2]} ~ ${weekDay[6][0]}/${weekDay[6][1]}/${weekDay[6][2]}</div>`;
+        calendarTopElement.innerHTML += `<button id="calendarrightbutton" onclick="goNextWeek();"></button>`;
+        calendarTopElement.innerHTML += `<button id="calendarweeklybutton" onclick="onButtonWeekClick();">Weekly</button><button id="calendarmonthlybutton" onclick="onButtonMonthClick();">Monthly</button>`;
+
+        let tempStr = '';
+        tempStr += `<table id="calendarweeklytablehead"><tr>`;
+        tempStr += `<th id="calendarweeklyelementfirst">Time</th>`;
+
+        for (let i = 0; i < 7; i++) {
+            tempStr += `<th id="calendarweeklyelement">${weekDay[i][1]}/${weekDay[i][2]}</th>`;
+        }
+
+        tempStr += `</tr></table>`;
+        calendarTableHeadElement.innerHTML = tempStr;
+
+        tempStr = '';
+        tempStr += `<table id="calendarweeklytable">`;
+
+        for (let i = 0; i < 24; i++) {
+            tempStr += `<tr>`;
+            tempStr += `<th id="calendarweeklyelementfirst">${i}:00~<br>${i + 1}:00</th>`;
+            for (let j = 0; j < 7; j++) {
+                tempStr += `<th id="calendarweeklyelement">`
+                tempStr += `</th>`;
+            }
+            tempStr += `</tr>`;
+        }
+
+        tempStr += `</table>`;
+        weekElement.innerHTML = tempStr;
+
+    } else if (viewMode === 'Month') {
+        weekElement.style.display = 'none';
+        monthElement.style.display = 'block';
+
+        calendarTopElement.innerHTML = '';
+        weekElement.innerHTML = '';
+        monthElement.innerHTML = '';
+
+        calendarTopElement.innerHTML += `<button id="calendarleftbutton" onclick="goPreviousMonth();"></button>`;
+        calendarTopElement.innerHTML += `<div id="calendardate">${dayCurrent.getFullYear()}/${dayCurrent.getMonth() + 1}</div>`;
+        calendarTopElement.innerHTML += `<button id="calendarrightbutton" onclick="goNextMonth();"></button>`;
+        calendarTopElement.innerHTML += `<button id="calendarrightbutton" onclick="goNextWeek();"></button>`;
+        calendarTopElement.innerHTML += `<button id="calendarweeklybutton" onclick="onButtonWeekClick();">Weekly</button><button id="calendarmonthlybutton" onclick="onButtonMonthClick();">Monthly</button>`;
+    }
+}
+
+function onButtonWeekClick() {
+    viewMode = 'Week';
+
+    findCalendarDate();
+    refreshCalendar();
+}
+
+function onButtonMonthClick() {
+    viewMode = 'Month';
+
+    findCalendarDate();
+    refreshCalendar();
+}
+
+function goPreviousWeek() {
+    dayCurrent.setDate(dayCurrent.getDate() - 7);
+
+    findCalendarDate();
+    refreshCalendar();
+}
+
+function goNextWeek() {
+    dayCurrent.setDate(dayCurrent.getDate() + 7);
+
+    findCalendarDate();
+    refreshCalendar();
+}
+
+function goPreviousMonth() {
+    dayCurrent.setMonth(dayCurrent.getMonth() - 1);
+
+    findCalendarDate();
+    refreshCalendar();
+}
+
+function goNextMonth() {
+    dayCurrent.setMonth(dayCurrent.getMonth() + 1);
+
+    findCalendarDate();
+    refreshCalendar();
 }
 
 function calendarEnd() {
--- Script/Dashboard.js
+++ Script/Dashboard.js
@@ -41,6 +41,28 @@ function dashboardRefresh() {
     let date = d.getDate();
     let dateText = year + '/' + month + '/' + date;
     document.getElementById('dashboarddatetext').innerHTML = dateText;
+
+    let today = new Date();
+    let tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    let todayDate = [today.getFullYear(), today.getMonth() + 1, today.getDate()];
+    let tomorrowDate = [tomorrow.getFullYear(), tomorrow.getMonth() + 1, tomorrow.getDate()];
+
+    let todayContent = document.getElementById('dashboardtodaycontent');
+    let tomorrowContent = document.getElementById('dashboardtomorrowcontent');
+
+    todayContent.innerHTML = '';
+    tomorrowContent.innerHTML = '';
+
+    for (let i = 0; i < usernameInfo['Plan'].length; i++) {
+        if (todayDate[0] === usernameInfo['Plan'][i]['Time'][0] && todayDate[1] === usernameInfo['Plan'][i]['Time'][1] && todayDate[2] === usernameInfo['Plan'][i]['Time'][2]) {
+            todayContent.innerHTML += `<div class="planinstance">${usernameInfo['Plan'][i]['Category']}/${usernameInfo['Plan'][i]['Title']}/${usernameInfo['Plan'][i]['Time']}</div>`;
+        }
+
+        if (tomorrowDate[0] === usernameInfo['Plan'][i]['Time'][0] && tomorrowDate[1] === usernameInfo['Plan'][i]['Time'][1] && tomorrowDate[2] === usernameInfo['Plan'][i]['Time'][2]) {
+            tomorrowContent.innerHTML += `<div class="planinstance">${usernameInfo['Plan'][i]['Category']}/${usernameInfo['Plan'][i]['Title']}/${usernameInfo['Plan'][i]['Time']}</div>`;
+        }
+    }
 }
 
 function commandKeyUp(event) {
@@ -50,8 +72,204 @@ function commandKeyUp(event) {
 }
 
 function addPlan() {
-    console.log(commandTextbox.value);
+    let today = new Date();
+    let command = commandTextbox.value.split(' ').slice(0, 4);
+
+    if (command.length === 3) {
+        let plan = {
+            'Category' : '',
+            'Title' : '',
+            'Time' : [1970, 01, 01, 03, 00, 00],
+            'Status' : 'NotAchieved'
+        };
+
+        plan['Category'] = command[0];
+        plan['TItle'] = command[1];
+
+        if (command[2] === 'today') {
+            plan['Time'][0] = today.getFullYear();
+            plan['TIme'][1] = today.getMonth();
+            plan['TIme'][2] = today.getDate();
+            plan['Time'][3] = today.getDay();
+        } else if (command[2] === 'tomorrow') {
+            today.setDate(todat.getDate() + 1);
+            plan['Time'][0] = today.getFullYear();
+            plan['TIme'][1] = today.getMonth();
+            plan['TIme'][2] = today.getDate();
+            plan['Time'][3] = today.getDay();
+        } else {
+            let returnedDate = dateChecker(command[2]);
+
+            if (returnedDate === null) {
+                plan['Time'][0] = today.getFullYear();
+                plan['TIme'][1] = today.getMonth();
+                plan['TIme'][2] = today.getDate();
+                plan['Time'][3] = today.getDay();
+            } else {
+                let selectedDate = new Date(returnedDate[0], returnedDate[1], returnedDate[2]);
+                plan['Time'][0] = selectedDate.getFullYear();
+                plan['Time'][1] = selectedDate.getMonth();
+                plan['Time'][2] = selectedDate.getDate();
+                plan['Time'][3] = selectedDate.getDay();
+            }
+        }
+
+        plan['Time'][4] = -1;
+        plan['Time'][5] = -1;
+
+        usernameInfo['Plan'].push(plan);
+    } else if (command.length === 4) {
+        let plan = {
+            'Category' : '',
+            'Title' : '',
+            'Time' : [1970, 01, 01, 03, 00, 00],
+            'Status' : 'NotAchieved'
+        };
+
+        plan['Category'] = command[0];
+        plan['Title'] = command[1];
+
+        if (command[2] === 'today') {
+            plan['Time'][0] = today.getFullYear();
+            plan['TIme'][1] = today.getMonth();
+            plan['TIme'][2] = today.getDate();
+            plan['Time'][3] = today.getDay();
+        } else if (command[2] === 'tomorrow') {
+            today.setDate(today.getDate() + 1);
+            plan['Time'][0] = today.getFullYear();
+            plan['TIme'][1] = today.getMonth();
+            plan['TIme'][2] = today.getDate();
+            plan['Time'][3] = today.getDay();
+        } else {
+            let returnedDate = dateChecker(command[2]);
+
+            if (returnedDate === null) {
+                plan['Time'][0] = today.getFullYear();
+                plan['TIme'][1] = today.getMonth();
+                plan['TIme'][2] = today.getDate();
+                plan['Time'][3] = today.getDay();
+            } else {
+                let selectedDate = new Date(returnedDate[0], returnedDate[1], returnedDate[2]);
+                plan['Time'][0] = selectedDate.getFullYear();
+                plan['Time'][1] = selectedDate.getMonth();
+                plan['Time'][2] = selectedDate.getDate();
+                plan['Time'][3] = selectedDate.getDay();
+            }
+        }
+
+        const regTime = /^[0-9]?[0-9]?:[0-9]?[0-9]?$/;
+
+        if (regTime.test(command[3]) === true) {
+            let timeSplit = command[3].split(':');
+            let time = [parseInt(timeSplit[0]), parseInt(timeSplit[1])];
+            console.log(time);
+
+            if (time[0] >= 0 && time[0] <= 23 && time[1] >= 0 && time[1] <= 59) {
+                plan['Time'][4] = time[0];
+                plan['Time'][5] = time[1];
+            } else {
+                plan['Time'][4] = -1;
+                plan['Time'][5] = -1;
+            }
+        } else {
+            plan['Time'][4] = -1;
+            plan['Time'][5] = -1;
+        }
+
+        usernameInfo['Plan'].push(plan);
+    }
+
+    commandTextbox.value = '';
     dashboardRefresh();
+}
+
+function dateChecker(str) {
+    let today = new Date();
+    const regDateExDateYYYYMMDD = /^[0-9]?[0-9]?[0-9]?[0-9]?\/[0-9]?[0-9]?\/[0-9]?[0-9]?$/;
+    const regDateExDateYYMMDD = /^[0-9]?[0-9]?\/[0-9]?[0-9]?\/[0-9]?[0-9]?$/;
+    const regDateExDateMMDD = /^[0-9]?[0-9]?\/[0-9]?[0-9]?\/[0-9]?[0-9]?$/;
+    const regDateExDateDD = /^[0-9]?[0-9]?$/;
+    const monthDateNotLeap = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const monthDateLeap = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (regDateExDateYYYYMMDD.test(str) === true) {
+        let dateSplit = str.split('/');
+        let date = [parseInt(dateSplit[0]), parseInt(dateSplit[1]), parseInt(dateSplit[2])];
+        if (date[1] >= 1 && date[1] <= 12) {
+            let ly = leapYearCheck(date[0]);
+            if (ly === true) {
+                if (dateSplit[2] >= 1 && date[2] <= monthDateLeap[date[1]]) {
+                    return date;
+                }
+            } else {
+                if (date[2] >= 1 && date[2] <= monthDateNotLeap[date[1]]) {
+                    return date;
+                }
+            }
+        }
+    } else if (regDateExDateYYMMDD.test(str) === true) {
+        let dateSplit = str.split('/');
+        let date = [2000 + parseInt(dateSplit[0]), parseInt(dateSplit[1]), parseInt(dateSplit[2])];
+        if (date[1] >= 1 && date[1] <= 12) {
+            let ly = leapYearCheck(date[0]);
+            if (ly === true) {
+                if (dateSplit[2] >= 1 && date[2] <= monthDateLeap[date[1]]) {
+                    return date;
+                }
+            } else {
+                if (date[2] >= 1 && date[2] <= monthDateNotLeap[date[1]]) {
+                    return date;
+                }
+            }
+        }
+    } else if (regDateExDateMMDD.test(str) === true) {
+        let dateSplit = str.split('/');
+        let date = [today.getFullYear(), parseInt(dateSplit[0]), parseInt(dateSplit[1])];
+        if (date[1] >= 1 && date[1] <= 12) {
+            let ly = leapYearCheck(date[0]);
+            if (ly === true) {
+                if (dateSplit[2] >= 1 && date[2] <= monthDateLeap[date[1]]) {
+                    return date;
+                }
+            } else {
+                if (date[2] >= 1 && date[2] <= monthDateNotLeap[date[1]]) {
+                    return date;
+                }
+            }
+        }
+    } else if (regDateExDateDD.test(str) === true) {
+        let date = [today.getFullYear(), today.getMonth() + 1, parseInt(str)];
+        if (date[1] >= 1 && date[1] <= 12) {
+            let ly = leapYearCheck(date[0]);
+            if (ly === true) {
+                if (dateSplit[2] >= 1 && date[2] <= monthDateLeap[date[1]]) {
+                    return date;
+                }
+            } else {
+                if (date[2] >= 1 && date[2] <= monthDateNotLeap[date[1]]) {
+                    return date;
+                }
+            }
+        }
+    }
+
+    return null;
+}
+
+function leapYearCheck(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
 
 function moveToDashboard() {
